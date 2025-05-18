8c7181c4814841781909121df7699b575f5bd846
Sultan <sultan.akhtar701@gmail.com>
2025-05-18 08:13:31 +0100

    added signup page section

--- src/components/SignUpPage.js
+++ src/components/SignUpPage.js
@@ -1,106 +1,125 @@
 import React, { useState } from 'react';
-import { Box, TextField, Button, Typography, Container, Radio, RadioGroup, FormControlLabel, FormLabel } from '@mui/material';
+import { Box, TextField, Button, Typography, Container, ToggleButton, ToggleButtonGroup } from '@mui/material';
+import BusinessIcon from '@mui/icons-material/Business';
+import PersonIcon from '@mui/icons-material/Person';
 import './SignUpPage.css';
 
 const SignUpPage = () => {
-  const [step, setStep] = useState(1);
+  const [page, setPage] = useState(1);
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [userType, setUserType] = useState('');
 
   const handleNext = () => {
-    setStep(step + 1);
+    setPage(2);
   };
 
   const handleBack = () => {
-    setStep(step - 1);
+    setPage(1);
   };
 
-  const handleSubmit = (e) => {
+  const handleUserTypeChange = (event, newType) => {
+    if (newType !== null) {
+      setUserType(newType);
+    }
+  };
+
+  const handleSignUp = (e) => {
     e.preventDefault();
-    console.log("User Info:", { name, email, password, userType });
+    console.log("Signing up with", name, email, password, userType);
   };
 
   return (
     <div className="signup-background">
       <Container maxWidth="sm" className="signup-container">
-        <Typography variant="h4" align="center" className="signup-title" gutterBottom>
-          {step === 1 ? "Sign Up" : "User Type"}
+        <div className="signup-logo-container">
+          <img src="/BizMapLogo.png" alt="BizMap Logo" className="signup-logo" />
+        </div>
+        <Typography variant="h4" className="signup-title" gutterBottom>
+          {page === 1 ? "Create Your BizMap Account" : "Choose Your Profile"}
         </Typography>
-
-        <Box component="form" onSubmit={handleSubmit} className="signup-form">
-          {step === 1 && (
-            <>
-              <TextField
-                label="Name"
-                variant="outlined"
-                fullWidth
-                margin="normal"
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                required
-              />
-              <TextField
-                label="Email"
-                variant="outlined"
-                fullWidth
-                margin="normal"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                required
-              />
-              <TextField
-                label="Password"
-                type="password"
-                variant="outlined"
-                fullWidth
-                margin="normal"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                required
-              />
-              <Button
-                variant="contained"
-                fullWidth
-                onClick={handleNext}
-                className="signup-button"
-              >
-                Next
-              </Button>
-            </>
-          )}
-
-          {step === 2 && (
-            <>
-              <FormLabel component="legend" className="user-type-label">Select User Type</FormLabel>
-              <RadioGroup
-                value={userType}
-                onChange={(e) => setUserType(e.target.value)}
-                className="user-type-group"
-              >
-                <FormControlLabel value="Business" control={<Radio />} label="Business" />
-                <FormControlLabel value="Individual" control={<Radio />} label="Individual" />
-              </RadioGroup>
-              <Button
-                variant="contained"
-                fullWidth
-                onClick={handleBack}
-                className="back-button"
-              >
-                Back
-              </Button>
-              <Button
-                type="submit"
-                variant="contained"
-                fullWidth
-                className="signup-button"
-              >
-                Sign Up
-              </Button>
-            </>
-          )}
-        </Box>
+        {page === 1 ? (
+          <Box component="form" onSubmit={handleNext} sx={{ mt: 2 }}>
+            <TextField
+              label="Name"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+            <TextField
+              label="Email"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            <TextField
+              label="Password"
+              type="password"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button
+              type="button"
+              variant="contained"
+              color="primary"
+              fullWidth
+              className="signup-button"
+              onClick={handleNext}
+            >
+              Next
+            </Button>
+          </Box>
+        ) : (
+          <Box component="form" onSubmit={handleSignUp} sx={{ mt: 2 }}>
+            <Typography variant="h6" gutterBottom>
+              Are you a business or an individual?
+            </Typography>
+            <ToggleButtonGroup
+              value={userType}
+              exclusive
+              onChange={handleUserTypeChange}
+              fullWidth
+              className="user-type-group"
+              sx={{ mb: 2 }}
+            >
+              <ToggleButton value="business" className="toggle-button">
+                <BusinessIcon sx={{ mr: 1 }} />
+                Business
+              </ToggleButton>
+              <ToggleButton value="individual" className="toggle-button">
+                <PersonIcon sx={{ mr: 1 }} />
+                Individual
+              </ToggleButton>
+            </ToggleButtonGroup>
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              fullWidth
+              className="signup-button"
+            >
+              Sign Up
+            </Button>
+            <Button
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              className="signup-button"
+              onClick={handleBack}
+              sx={{ mt: 1 }}
+            >
+              Back
+            </Button>
+          </Box>
+        )}
       </Container>
     </div>
   );
